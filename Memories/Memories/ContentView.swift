40d62801145351f29331160779c1ae9439900bde
xyz <81219723@qq.com>
2024-09-10 21:50:06 +0800

    lesson1

---
+++ Memories/Memories/ContentView.swift
@@ -0,0 +1,46 @@
+//
+//  ContentView.swift
+//  Memories
+//
+//  Created by xyz on 2024/9/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
+        }
+        .foregroundColor(.orange)
+        .imageScale(.large)
+        .padding()
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    var body: some View {
+        ZStack{
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth: 2)
+                Text("👻")
+                    .font(.largeTitle)
+            } else
+            {
+                RoundedRectangle(cornerRadius: 12)
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
---
+++ Memories/Memories/MemoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoriesApp.swift
+//  Memories
+//
+//  Created by xyz on 2024/9/10.
+//
+
+import SwiftUI
+
+@main
+struct MemoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
